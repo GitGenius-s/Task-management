f5f46ed739e15e2e953cd1f1acc81f8ad5930198
GitGenius-s <codemastermind1234@gmail.com>
2024-02-26 14:39:45 +0530

    passport authorization

--- Task/app/Http/Controllers/Auth/RegisterController.php
+++ Task/app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -43,10 +44,10 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     *
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    protected function validator($data)
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
@@ -58,15 +59,16 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * 
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create(Request $request)
     {
+        // dd($request->all());
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
     }
 }
--- Task/routes/api.php
+++ Task/routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('ya',function(){
+    return 'hi';
+});
+
+Route::middleware('auth:api')->group(function () {
+    // Routes that require authentication
+    Route::post('user', function(){
+        return "Authorized user!";
+    });
+   
+});
+
+Route::post('login', 'Auth\LoginController@login');
+
+Route::get('home', function () {
+    return response('Hello World', 200)
+                  ->header('Content-Type', 'text/plain');
+});
+
+Route::post('register', 'Auth\RegisterController@create');
